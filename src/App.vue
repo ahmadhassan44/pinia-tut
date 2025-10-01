f0e3cf6d2c144572029193901775f402ee3ddb44
Ahmad Hassan <ahmadhassantanveer44@gmail.com>
2025-10-01 19:53:09 +0500

    feat/add assets and setup app componennt

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,10 @@
 <script setup lang="ts"></script>
 
 <template>
-  <h1>You did it!</h1>
-  <p>
-    Visit <a href="https://vuejs.org/" target="_blank" rel="noopener">vuejs.org</a> to read the
-    documentation
-  </p>
+  <header>
+    <img src="./assets/pinia-logo.svg" alt="logo" class="logo" />
+    <h1>Pinia Tasks</h1>
+  </header>
 </template>
 
 <style scoped></style>
